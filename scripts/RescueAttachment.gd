534a3ba9c32a2e172f50917160a219b5fc1a9beb
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-08-23 00:02:16 -0500

    Added RescueAttachment and InputNode

---
+++ scripts/RescueAttachment.gd
@@ -0,0 +1,41 @@
+## This [Node] will keep its parent [Node] in place even if that parent's parent [Node] is destroyed.
+class_name RescueAttachment extends Node
+
+signal rescued
+
+var node : Node :
+	get: return self
+
+var parent : Node :
+	get: return node.get_parent()
+
+var times_rescued : int = 0
+
+func _ready() -> void:
+	parent.tree_exiting.connect(rescue)
+
+func rescue() -> void:
+	var gt
+	var apply_global := node is Node2D or node is Node3D
+	if apply_global:
+		gt = node.transform
+
+	var ancestor : Node = parent
+	while ancestor.is_queued_for_deletion():
+		if apply_global:
+			gt = ancestor.transform * gt
+		ancestor = ancestor.get_parent()
+
+	parent.remove_child(node)
+	ancestor.add_child.call_deferred(node)
+
+	if apply_global:
+		node.transform = gt
+
+	times_rescued += 1
+	rescued.emit()
+
+func queue_free_if_rescued() -> void:
+	if times_rescued == 0: return
+
+	queue_free()
---
+++ scripts/RescueAudioStreamPlayer.gd
@@ -0,0 +1,12 @@
+## Special [RescueAttachment] intended for use with [AudioStreamPlayer]s. If not playing, this [Node] will instantly delete. Otherwise, it will be rescued until the audio is finished playing.
+extends RescueAttachment
+
+func rescue() -> void:
+	super.rescue()
+	post_rescue.call_deferred()
+
+func post_rescue() -> void:
+	if node.playing:
+		await node.finished
+
+	node.queue_free()
