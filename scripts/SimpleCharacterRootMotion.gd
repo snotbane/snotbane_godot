4280c3979d51e06b1abb44b8c1be94c8e74b4e60
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-09-01 16:46:45 -0500

    Added SimpleCharacterRootMotion

---
+++ scripts/SimpleCharacterRootMotion.gd
@@ -0,0 +1,17 @@
+extends RootMotionView
+
+@onready var character : CharacterBody3D = self.get_parent()
+@onready var anim_mixer : AnimationMixer = self.get_node(animation_path)
+
+var root_velocity_global : Vector3
+
+func _process(delta: float) -> void:
+	var root_position := anim_mixer.get_root_motion_position()
+	root_velocity_global = Quaternion.from_euler(character.global_rotation) * root_position / delta
+
+
+func _physics_process(delta: float) -> void:
+	if root_velocity_global:
+		character.velocity = root_velocity_global
+		# character.move_and_collide(root_velocity_global)
+
