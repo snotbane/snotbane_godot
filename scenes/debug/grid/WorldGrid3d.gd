bb4bbba5da96f09e0354269a43c965ea34cac957
snotbane <27900656+snotbane@users.noreply.github.com>
2025-08-31 19:46:41 -0500

    Fixed input string name used

--- scenes/debug/grid/WorldGrid3d.gd
+++ scenes/debug/grid/WorldGrid3d.gd
@@ -7,5 +7,5 @@ func _ready() -> void:
 		visible = visible and OS.has_feature(&"editor_runtime")
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed(&"debug_grid_toggle"):
+	if event.is_action_pressed(Mincuz.INPUT_DEBUG_GRID_TOGGLE):
 		visible = not visible
