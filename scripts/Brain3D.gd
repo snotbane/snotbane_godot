f196140297374dfe3ba95006933da63ebd2bb962
snotbane <27900656+snotbane@users.noreply.github.com>
2025-10-29 01:33:31 -0500

    Refactor Brain3D and BrainTarget classes for improved navigation handling and target assignment

--- scripts/Brain3D.gd
+++ scripts/Brain3D.gd
@@ -3,20 +3,41 @@ class_name Brain3D extends NavigationAgent3D
 
 signal desired_move(direction: Vector3)
 
-@export_range(0.01, 1.0, 0.01, "or_greater") var nav_refresh_duration : float = 0.2
-@export var continuous_update : bool = false
+@export_subgroup("Navigation")
 
-var nav_target : BrainTarget
+var _refresh_duration : float = 0.1
+## If greater than 0.0, [member target_position] will be continuously updated after this many seconds. If 0.0, this will only occur when [member target] is manually changed or [member refresh_target_position] is manually called.
+@export_range(0.0, 1.0, 0.01, "or_greater") var refresh_duration : float = 0.1 :
+	get: return _refresh_duration
+	set(value):
+		if _refresh_duration == value: return
+		_refresh_duration = value
+
+		if target != null:
+			target.refresh_duration = _refresh_duration
+
+
+
+
+## If enabled, [member target_position] will be updated once every [member refresh_duration] seconds.
+var continuous_update : bool :
+	get: return refresh_duration > 0.0
+
+
+
+var target : BrainTarget
+
+var _move_vector_prev : Vector3
+
+
 var travelling : bool :
-	get: return nav_target.is_assigned and not is_target_reached()
+	get: return target.is_assigned and not is_navigation_finished()
+
 
 func _ready() -> void:
-	nav_target = BrainTarget.new(nav_refresh_duration)
-	if continuous_update:
-		nav_target.updated.connect(refresh_target_position)
-	add_child(nav_target)
-
-	# target_reached.connect(print.bind("Target reached"))
+	target = BrainTarget.new(refresh_duration)
+	target.changed.connect(refresh_target_position)
+	target.updated.connect(refresh_target_position)
 
 	sequence()
 
@@ -26,29 +47,33 @@ func sequence() -> void:
 	while is_instance_valid(self): await call(&"_sequence")
 
 func _physics_process(delta: float) -> void:
+	var move_vector : Vector3
 	if travelling:
-		var displacement = (get_next_path_position() - get_parent().global_position) * Vector3(1, 0, 1)
-		desired_move.emit(displacement.normalized())
+		move_vector = ((get_next_path_position() - get_parent().global_position) * Vector3(1, 0, 1)).normalized()
 	else:
-		desired_move.emit(Vector3.ZERO)
+		move_vector = Vector3.ZERO
+
+	if move_vector != _move_vector_prev:
+		desired_move.emit(move_vector)
+		_move_vector_prev = move_vector
 
 
 func refresh_target_position() -> void:
-	target_position = nav_target.target_position
+	target_position = target.target_position
 
 func travel(target: Variant) :
 	if target != null:
 		var was_travelling := travelling
-		nav_target.target = target
+		target.target = target
 		refresh_target_position()
 		if not was_travelling:
-			await wait(nav_refresh_duration)
+			await wait(refresh_duration)
 			if not is_target_reached():
 				await target_reached
 	stop()
 
 func stop() -> void:
-	nav_target.target = null
+	target.target = null
 
 func wait(duration_seconds: float) :
 	await get_tree().create_timer(duration_seconds).timeout
--- scripts/BrainTarget.gd
+++ scripts/BrainTarget.gd
@@ -1,67 +1,70 @@
 class_name BrainTarget extends Timer
 
+## Emitted when the target node or position is changed.
+signal changed
+## Emitted whenever the timer runs out, prompting a navigation update.
 signal updated
-
-var host : Node
 
 var _target_pos : Variant
 var _target_node : Node
-## The position or Node which the Brain will try to reach.
-var target : Variant :
-	get: return _target_node
-	set(value):
-		assert(value == null or value is Node2D or value is Node3D or value is Vector2 or value is Vector3, "Assigned target must be a Node2D or Node3D, or a Vector2 or Vector3s.")
-		if _target_node == value: return
-		var value_is_node : bool = value is Node3D or value is Node2D
-
-		if _target_node is Node3D or _target_node is Node2D:
-			_target_node.tree_exiting.disconnect(recall)
-
-		_target_node = value if value_is_node else null
-		_target_pos = Vector3.ZERO if _target_node is Node3D else Vector2.ZERO
-
-		if _target_node is Node3D or _target_node is Node2D:
-			_target_node.tree_exiting.connect(recall)
-
-		var new_parent : Node = (_target_node if _target_node is Node2D or _target_node is Node3D else host)
-		if get_parent() != new_parent:
-			reparent(new_parent, false)
 
 ## The global position of the target.
 var target_position : Variant :
 	get:
 		assert(is_assigned, "BrainTarget must be assigned in order to get the target position.")
-		var parent := get_parent()
-		if parent is Node2D or parent is Node3D:
-			return parent.to_global(_target_pos)
+		if _target_node is Node2D or _target_node is Node3D:
+			return _target_node.to_global(_target_pos)
 		else:
 			return _target_pos
 
 ## Returns true if the target is currently assigned.
 var is_assigned : bool :
-	get: return target != null
+	get: return _target_node != null or _target_pos != null
+
+var is_assigned_to_node : bool :
+	get: return _target_node != null
+
+var _wait_time_is_valid : bool
+## Effectively the same as [member wait_time], but it allows for a value of 0.0.
+var refresh_duration : float :
+	get: return wait_time if _wait_time_is_valid else 0.0
+	set(value):
+		_wait_time_is_valid = value > 0.0
+		if _wait_time_is_valid:
+			wait_time = value
+			if is_stopped(): start()
+		else:
+			if not is_stopped(): stop()
 
 
 func _init(__wait_time__: float) -> void:
 	process_callback = Timer.TIMER_PROCESS_PHYSICS
-	wait_time = __wait_time__
-	autostart = true
 	timeout.connect(try_update)
-
-func _enter_tree() -> void:
-	var parent := get_parent()
-	if parent is NavigationAgent3D or parent is NavigationAgent2D:
-		host = parent
+	refresh_duration = __wait_time__
 
 func try_update() -> void:
 	if not is_assigned: return
 	updated.emit()
 
-func recall() -> void:
-	target = null
+func assign(value: Variant, relative_pos: Variant = null) -> void:
+	assert(value == null or value is Node2D or value is Node3D or value is Vector2 or value is Vector3, "Assigned target must be a Node2D or Node3D, or a Vector2 or Vector3.")
+	assert(relative_pos == null or relative_pos is Vector2 or relative_pos is Vector3, "Relative position must be a Vector2 or Vector3.")
+	if _target_node == value and _target_pos == relative_pos: return
 
-func assign(__target__: Variant, relative: Variant = null) -> void:
-	target = __target__
+	var value_is_node : bool = value is Node3D or value is Node2D
 
-	if relative != null:
-		_target_pos = relative
+	if _target_node != null:
+		_target_node.tree_exiting.disconnect(unassign)
+
+	_target_node = value if value_is_node else null
+	_target_pos = ((Vector3.ZERO if _target_node is Node3D else Vector2.ZERO) if value_is_node else value)
+	if relative_pos != null:
+		_target_pos += relative_pos
+
+	if _target_node != null:
+		_target_node.tree_exiting.connect(unassign)
+
+	changed.emit()
+
+func unassign() -> void:
+	assign(null)
