33c39e05a7e4f43d33eaec91c866a8460c635877
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-10-29 02:04:45 -0500

    Reorganized files

--- scripts/InputNode.gd
+++
@@ -1,8 +0,0 @@
-class_name InputNode extends Node
-
-@export var _enabled : bool = true
-var enabled : bool :
-	get:
-		if get_parent() is InputNode:
-			return _enabled and get_parent().enabled
-		return _enabled
