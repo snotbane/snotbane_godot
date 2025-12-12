ed91197f07e73049b1692808dda06d106d591044
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-12-11 19:04:25 -0600

    Added descriptions

--- modules/inventory/ItemDefinition.gd
+++ modules/inventory/ItemDefinition.gd
@@ -2,6 +2,11 @@
 ## Generic class for user-facing, static object data.
 class_name ItemDefinition extends Resource
 
+## User-facing icon.
 @export var icon : Texture2D
+
+## User-facing name.
 @export var name : String
+
+## User-facing description.
 @export_multiline var description : String
